35ed1e77356220343c6d062aa5dade76241ec9a4
yigitaksoy <yigitaksoy@gmail.com>
2023-03-15 22:15:19 +0300

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./assets/css/style.css";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <div className="grid h-screen place-content-center bg-black">
-      <h3 className="text-center font-fontNove text-6xl font-bold text-white">
-        Welcome to <span className="text-cyan-400">SpeakEasy!</span>
-      </h3>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
