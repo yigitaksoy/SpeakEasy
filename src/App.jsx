1730777dafde08983c1955d7f7317991e65db140
yigitaksoy <yigitaksoy@gmail.com>
2023-03-15 22:19:48 +0300

    Update route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./assets/css/style.css";
-import Home from "./pages/Home";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
+import Home from "./routes/Home";
+import Register from "./routes/Register";
+import Login from "./routes/Login";
 
 const App = () => {
   return (
