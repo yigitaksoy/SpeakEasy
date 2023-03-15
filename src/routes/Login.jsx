555fbaf7a715f810becf13fe45a91e44e030f1a3
yigitaksoy <yigitaksoy@gmail.com>
2023-03-16 02:52:40 +0300

    Remove unused classes and center title

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
         </Link>
         <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0">
           <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               Welcome back!
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
@@ -32,7 +32,7 @@ const Login = () => {
                   type="text"
                   name="username"
                   id="username"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
+                  className="block  w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
                   placeholder="username"
                   required
                 />
@@ -87,7 +87,7 @@ const Login = () => {
                 Don’t have an account yet?
                 <Link
                   to="/register"
-                  class="font-black text-black hover:underline"
+                  className="font-black text-black hover:underline"
                 >
                   Sign up
                 </Link>
@@ -99,7 +99,7 @@ const Login = () => {
               <div className="flex flex-col space-y-4">
                 <Link
                   to="#"
-                  class="group flex items-center justify-center space-x-2 rounded-md border border-gray-800 px-4 py-2 transition-colors duration-300 hover:bg-gray-800 focus:outline-none"
+                  className="group flex items-center justify-center space-x-2 rounded-md border border-gray-800 px-4 py-2 transition-colors duration-300 hover:bg-gray-800 focus:outline-none"
                 >
                   <span>
                     <svg
@@ -122,7 +122,7 @@ const Login = () => {
               <div className="flex flex-col space-y-4">
                 <Link
                   to="#"
-                  class="group flex items-center justify-center space-x-2 rounded-md border border-gray-800 px-4 py-2 transition-colors duration-300 hover:bg-gray-800 focus:outline-none"
+                  className="group flex items-center justify-center space-x-2 rounded-md border border-gray-800 px-4 py-2 transition-colors duration-300 hover:bg-gray-800 focus:outline-none"
                 >
                   <svg
                     className="h-5 w-5 fill-current text-gray-800 group-hover:text-white"
