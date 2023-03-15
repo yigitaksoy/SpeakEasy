8e39020432859298030268ce7c87330958a4aee2
yigitaksoy <yigitaksoy@gmail.com>
2023-03-16 01:44:34 +0300

    Add username field for login form

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -26,14 +26,14 @@ const Login = () => {
                   htmlFor="email"
                   className="mb-2 block text-sm font-medium text-gray-900 "
                 >
-                  Your email
+                  Username
                 </label>
                 <input
-                  type="email"
-                  name="email"
-                  id="email"
+                  type="text"
+                  name="username"
+                  id="username"
                   className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
-                  placeholder="name@email.com"
+                  placeholder="username"
                   required
                 />
               </div>
