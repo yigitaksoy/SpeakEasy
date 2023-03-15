4bd47edb02cdf9a3c76e4b1edfbe13dca0a882af
yigitaksoy <yigitaksoy@gmail.com>
2023-03-15 22:18:11 +0300

    Add homepage

---
+++ src/routes/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <section className="grid h-screen place-content-center bg-black">
+      <h3 className="text-center font-fontNove font-bold text-white lg:text-6xl">
+        Welcome to <span className="text-cyan-400">SpeakEasy!</span>
+      </h3>
+    </section>
+  );
+};
+
+export default Home;
