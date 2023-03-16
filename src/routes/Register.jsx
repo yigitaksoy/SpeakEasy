d0a1118b97171728228f84c59b4ee2057dc22d6d
yigitaksoy <yigitaksoy@gmail.com>
2023-03-16 12:12:08 +0300

    Add password validation

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,6 +1,36 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
+      setEmail(e.target.value);
+    } else if (e.target.name === "password") {
+      setPassword(e.target.value);
+    } else if (e.target.name === "confirmPassword") {
+      setConfirmPassword(e.target.value);
+      setPasswordsMatch(e.target.value === password);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log("username: ", e.target[0].value);
+    console.log("Email: ", e.target[1].value);
+    console.log("Password: ", e.target[2].value);
+  };
+
+  useEffect(() => {
+    setPasswordsMatch(password === confirmPassword);
+  }, [password, confirmPassword]);
+
   return (
     <section className="h-auto bg-sky-400 font-fontInforma">
       <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-auto md:py-3.5">
@@ -17,10 +47,14 @@ const Register = () => {
         </Link>
         <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0">
           <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              Register
+            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              Create an account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form
+              method="POST"
+              className="space-y-4 md:space-y-6"
+              onSubmit={handleSubmit}
+            >
               <div>
                 <label
                   htmlFor="username"
@@ -40,7 +74,7 @@ const Register = () => {
               <div>
                 <label
                   htmlFor="email"
-                  className="mb-2 block text-sm font-medium text-gray-900 "
+                  className="mb-2 block text-sm font-medium text-gray-900"
                 >
                   Your email
                 </label>
@@ -65,25 +99,45 @@ const Register = () => {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
+                  className={`block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm ${
+                    !passwordsMatch ? " focus:text-red-500" : ""
+                  }`}
+                  pattern=".{8,}"
+                  title="8 characters minimum"
                   required
+                  onChange={handleChange}
                 />
+                <p className="text-xs text-neutral-700">
+                  Passwords must be min. 8 characters
+                </p>
               </div>
               <div>
                 <label
-                  htmlFor="password"
+                  htmlFor="confirmPassword"
                   className="mb-2 block text-sm font-medium text-gray-900 "
                 >
                   Confirm Password
                 </label>
                 <input
-                  type="confirm-password"
-                  name="confirm-password"
-                  id="confirm-password"
+                  type="password"
+                  name="confirmPassword"
+                  id="confirmPassword"
                   placeholder="••••••••"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
+                  className={`block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm ${
+                    !passwordsMatch ? "focus:text-red-500" : ""
+                  }`}
                   required
+                  pattern=".{8,}"
+                  title="8 characters minimum"
+                  onChange={(e) => setConfirmPassword(e.target.value)}
                 />
+                {!passwordsMatch ? (
+                  <p className="text-xs text-red-500">
+                    Passwords do not match!
+                  </p>
+                ) : (
+                  ""
+                )}
               </div>
               <div className="flex items-center justify-between">
                 <div className="flex items-start">
@@ -93,7 +147,6 @@ const Register = () => {
                       aria-describedby="remember"
                       type="checkbox"
                       className="focus:ring-3 focus:ring-primary-300  h-4 w-4 rounded border border-gray-300 bg-gray-50"
-                      required
                     />
                   </div>
                   <div className="ml-3 text-sm">
@@ -107,11 +160,14 @@ const Register = () => {
                 type="submit"
                 className="w-full rounded-lg bg-black px-5 py-2.5  text-center text-sm font-medium text-white hover:bg-neutral-800  focus:outline-none focus:ring-4 focus:ring-violet-300"
               >
-                Create an account
+                Sign up
               </button>
               <p className="text-sm font-light text-gray-500 ">
                 Already have an account?
-                <Link to="/login" class="font-black text-black hover:underline">
+                <Link
+                  to="/login"
+                  className="font-black text-black hover:underline"
+                >
                   Log in
                 </Link>
               </p>
@@ -122,7 +178,7 @@ const Register = () => {
               <div className="flex flex-col space-y-4">
                 <Link
                   to="#"
-                  class="group flex items-center justify-center space-x-2 rounded-md border border-gray-800 px-4 py-2 transition-colors duration-300 hover:bg-gray-800 focus:outline-none"
+                  className="group flex items-center justify-center space-x-2 rounded-md border border-gray-800 px-4 py-2 transition-colors duration-300 hover:bg-gray-800 focus:outline-none"
                 >
                   <span>
                     <svg
@@ -145,7 +201,7 @@ const Register = () => {
               <div className="flex flex-col space-y-4">
                 <Link
                   to="#"
-                  class="group flex items-center justify-center space-x-2 rounded-md border border-gray-800 px-4 py-2 transition-colors duration-300 hover:bg-gray-800 focus:outline-none"
+                  className="group flex items-center justify-center space-x-2 rounded-md border border-gray-800 px-4 py-2 transition-colors duration-300 hover:bg-gray-800 focus:outline-none"
                 >
                   <svg
                     className="h-5 w-5 fill-current text-gray-800 group-hover:text-white"
