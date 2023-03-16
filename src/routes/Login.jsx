de4ee7b4777c1a2736e4423241f260e903c31fc4
yigitaksoy <yigitaksoy@gmail.com>
2023-03-16 15:31:27 +0300

    Add password validation pattern

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -48,7 +48,9 @@ const Login = () => {
                   type="password"
                   name="password"
                   id="password"
+                  pattern=".{8,}"
                   placeholder="••••••••"
+                  title="8 characters minimum"
                   className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
                   required
                 />
