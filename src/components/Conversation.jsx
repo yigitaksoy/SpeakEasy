a1cba66a043e4721cf1ac1a99fee1e647b8268d8
yigitaksoy <yigitaksoy@gmail.com>
2023-03-20 15:06:48 +0300

    Add signout functionality and adjust borders

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase/firebase";
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
 
 const Conversation = () => {
   return (
     <div>
-      <div className="relative flex items-center rounded-lg border-b border-gray-300 bg-sky-400 p-3">
+      <div className="relative flex items-center border-b bg-sky-400 p-3 md:rounded-tr-lg">
         <img
           className="h-10 w-10 rounded-full object-cover"
           src="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
@@ -11,6 +13,9 @@ const Conversation = () => {
         />
         <span className="ml-2 block font-bold text-black">Emma</span>
         <p className="absolute right-12 z-50 font-fontNove"> SpeakEasy </p>
+        <button onClick={() => signOut(auth)} className="absolute right-2">
+          <ArrowRightOnRectangleIcon className=" h-6 w-6 text-black" />
+        </button>
       </div>
       <div className="relative h-[40rem] w-full overflow-y-auto p-6 md:h-[35rem]">
         <ul className="space-y-2">
