a3508adc27c58a99a363a30e2f27828531120242
yigitaksoy <yigitaksoy@gmail.com>
2023-03-20 15:07:12 +0300

    Create navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="invisible relative flex h-16 flex-col justify-center bg-sky-400 text-center md:visible md:rounded-tl-lg">
+      <h3 className="font-fontNove text-lg text-black">SpeakEasy</h3>
+    </div>
+  );
+};
+
+export default Navbar;
