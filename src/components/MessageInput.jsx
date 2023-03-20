833bac0b7cb9eb733951a33adae40605dc761457
yigitaksoy <yigitaksoy@gmail.com>
2023-03-20 15:08:20 +0300

    Update input placeholder text

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -9,7 +9,7 @@ const MessageInput = () => {
 
       <input
         type="text"
-        placeholder="Message"
+        placeholder="Type a message"
         className="mx-3 block w-full rounded-full bg-gray-100 py-2 pl-4 outline-none focus:text-gray-700"
         name="message"
         required
