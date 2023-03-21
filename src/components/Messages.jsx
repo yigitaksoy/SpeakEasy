1466fa172346d252803d7bcd789b1faed8d9242b
yigitaksoy <yigitaksoy@gmail.com>
2023-03-21 03:29:54 +0300

    Get current users chats

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,30 +1,59 @@
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { doc, onSnapshot, setDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Messages = () => {
+  const [messages, setMessages] = useState([]);
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getMessages = () => {
+      const docRef = doc(db, "userChats", currentUser.uid);
+      const unsubscribe = onSnapshot(docRef, (doc) => {
+        if (doc.exists()) {
+          setMessages(doc.data());
+        } else {
+          setDoc(docRef, {});
+        }
+      });
+      return () => unsubscribe();
+    };
+    currentUser.uid && getMessages();
+  }, [currentUser.uid]);
+
   return (
     <ul className="overflow-auto sm:invisible md:visible">
       <h2 className="text-md my-2 mb-2 ml-2 font-fontNove text-black ">
         Chats
       </h2>
       <li>
-        <Link className="flex cursor-pointer items-center border-b border-gray-300 px-3 py-2 text-sm transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none">
-          <img
-            className="h-10 w-10 rounded-full object-cover"
-            src="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
-            alt="username"
-          />
-          <div className="w-full pb-2">
-            <div className="flex justify-between">
-              <span className="ml-2 block font-semibold text-gray-600">
-                Emma
+        {Object.entries(messages)?.map((message) => (
+          <Link
+            className="flex cursor-pointer items-center border-b border-gray-300 px-3 py-2 text-sm transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none"
+            key={message[0]}
+            onClick={() => console.log(message)}
+          >
+            <img
+              className="h-10 w-10 rounded-full object-cover"
+              src={message[1]?.userInfo.photoURL}
+              alt="username"
+            />
+            <div className="w-full pb-2">
+              <div className="flex justify-between">
+                <span className="ml-2 block font-semibold text-gray-600">
+                  {message[1]?.userInfo.displayName}
+                </span>
+                <span className="ml-2 block text-sm text-gray-600">6 hour</span>
+              </div>
+              <span className="ml-2 block text-sm text-gray-600">
+                {message[1]?.lastMessage?.text}
               </span>
-              <span className="ml-2 block text-sm text-gray-600">6 hour</span>
             </div>
-            <span className="ml-2 block text-sm text-gray-600">
-              Good Morning
-            </span>
-          </div>
-        </Link>
+          </Link>
+        ))}
       </li>
     </ul>
   );
