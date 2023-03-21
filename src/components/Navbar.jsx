eea8930f4bb5948d95f8a7a39af659461edd3641
yigitaksoy <yigitaksoy@gmail.com>
2023-03-21 20:30:47 +0300

    Change logo color and background, add heroicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
+import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/outline";
+
 const Navbar = () => {
   return (
-    <div className="invisible relative flex h-16 flex-col justify-center bg-sky-400 text-center md:visible md:rounded-tl-lg">
-      <h3 className="font-fontNove text-lg text-black">SpeakEasy</h3>
+    <div className="invisible relative flex h-16 flex-col justify-center bg-black text-center md:visible md:rounded-tl-lg">
+      <h3 className="flex items-center justify-center font-fontNove text-lg text-sky-400">
+        <span className="mr-2">SpeakEasy</span>
+        <ChatBubbleLeftRightIcon className="h-6 w-6 text-center" />
+      </h3>
     </div>
   );
 };
