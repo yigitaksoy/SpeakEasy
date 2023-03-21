242c7a66a9705f0134e94091123424f857620ac1
yigitaksoy <yigitaksoy@gmail.com>
2023-03-21 14:55:07 +0300

    Add user private chat functionality

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -1,11 +1,23 @@
-import { signOut } from "firebase/auth";
-import { auth } from "../firebase/firebase";
-import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
+import { signOut } from "firebase/auth";
+import { doc, onSnapshot } from "firebase/firestore";
+import { auth, db } from "../firebase/firebase";
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
 
 const Conversation = () => {
   const { data } = useContext(ChatContext);
+  const [messages, setMessages] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+
+    return () => unsubscribe();
+  }, [data.chatId]);
   return (
     <div>
       <div className="relative flex items-center border-b bg-sky-400 p-3 md:rounded-tr-lg">
@@ -23,18 +35,52 @@ const Conversation = () => {
         </button>
       </div>
       <div className="relative h-[40rem] w-full overflow-y-auto p-6 md:h-[35rem]">
-        <ul className="space-y-2">
-          <li className="flex justify-start">
-            <div className="relative max-w-xl rounded px-4 py-2 text-gray-700 shadow">
-              <span className="block">Received Message</span>
+        {messages.map((message) => {
+          const isCurrentUser = message.senderId === currentUser.uid;
+          const bubbleClass = isCurrentUser ? "bg-violet-100" : "bg-gray-200";
+          return (
+            <div
+              key={message.id}
+              className={`chat ${isCurrentUser ? "chat-end" : "chat-start"}`}
+            >
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    src={
+                      isCurrentUser ? currentUser.photoURL : data.user.photoURL
+                    }
+                    alt={`${
+                      isCurrentUser
+                        ? currentUser.displayName
+                        : data.user.displayName
+                    } profile`}
+                  />
+                </div>
+              </div>
+              <div className="chat-header">
+                {isCurrentUser
+                  ? currentUser.displayName
+                  : data.user.displayName}
+
+                <time className="ml-2 text-xs opacity-50">{/* Time */}</time>
+              </div>
+              <div className={`chat-bubble text-black ${bubbleClass}`}>
+                {message.text}
+              </div>
+              <div className="chat-footer text-xs opacity-50">
+                <time className="text-xs opacity-50">
+                  {new Date(message.date.seconds * 1000).toLocaleTimeString(
+                    [],
+                    {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    }
+                  )}
+                </time>
+              </div>
             </div>
-          </li>
-          <li className="flex justify-end">
-            <div className="relative max-w-xl rounded bg-gray-100 px-4 py-2 text-gray-700 shadow">
-              <span className="block">Sent Message</span>
-            </div>
-          </li>
-        </ul>
+          );
+        })}
       </div>
     </div>
   );
